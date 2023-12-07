3a2d299861dd3854219f6a4fb0a2294fd8970c23
CanErsoy20 <can.ersoy@ug.bilkent.edu.tr>
2023-12-07 20:12:42 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/cs411_project2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cs411_project2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Browser(),
+    );
+  }
+}
+
+class Browser extends StatefulWidget {
+  const Browser({super.key});
+
+  @override
+  State<Browser> createState() => _BrowserState();
+}
+
+class _BrowserState extends State<Browser> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Container(
+        color: Colors.red,
+        height: 30,
+      )),
+    );
+  }
+}
